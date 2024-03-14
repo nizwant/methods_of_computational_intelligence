2be486fef267fa6fa8c97e2382002f62216bdc3e
nizwant <mateusznizwantowski@gmail.com>
2024-03-14 13:16:25 +0100

    mini batch with momentum

--- NeuralNetworks/implementation/optimizers.py
+++ NeuralNetworks/implementation/optimizers.py
@@ -89,3 +89,58 @@ class mini_batch_gradient_descent(Optimizer):
                 current_solution = current_solution - learning_rate * gradient
             print("Epoch:", current_solution)
         return current_solution
+
+
+class mini_batch_gradient_descent_with_momentum(Optimizer):
+    def optimize(
+        self,
+        X,
+        y,
+        initial_solution,
+        calculate_gradient,
+        learning_rate=0.01,
+        momentum_decay=0.9,
+        max_num_epoch=1000,
+        batch_size=1,
+        batch_fraction=None,
+    ):
+        """
+        Performs mini batch gradient descent with momentum optimization.
+
+        Parameters:
+        - X: Input data.
+        - y: Target labels.
+        - initial_solution: Initial solution for optimization.
+        - calculate_gradient: Function to calculate the gradient.
+        - learning_rate: Learning rate for updating the solution (default: 0.01).
+        - momentum_decay: Decay rate for the momentum (default: 0.9).
+        - max_num_iters: Maximum number of iterations (default: 1000).
+        - batch_size: Size of the mini batch (default: 1).
+        - batch_fraction: Fraction of the data to use in each mini batch (default: None).
+
+        Returns:
+        - The optimized solution.
+        """
+
+        X, y = self.transfer_data_to_numpy(X, y)
+        batch_size, iterations = self.calculate_batch_size_and_iteration(
+            batch_size, batch_fraction, X
+        )
+
+        current_solution = initial_solution
+        momentum = np.zeros_like(initial_solution)
+
+        for _ in range(max_num_epoch):
+            N, _ = X.shape
+            shuffled_idx = np.random.permutation(N)
+            X, y = X[shuffled_idx], y[shuffled_idx]
+            for idx in range(iterations):
+                X_selected, y_selected = (
+                    X[idx * batch_size : (idx + 1) * batch_size],
+                    y[idx * batch_size : (idx + 1) * batch_size],
+                )
+                gradient = calculate_gradient(X_selected, y_selected, current_solution)
+                momentum = momentum_decay * momentum - learning_rate * gradient
+                current_solution = current_solution + momentum
+            print("Epoch:", current_solution)
+        return current_solution
