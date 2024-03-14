f063ade9b94b406d60953255f7818ebd12977283
nizwant <mateusznizwantowski@gmail.com>
2024-03-14 13:26:13 +0100

    rmsprops and adam

--- NeuralNetworks/implementation/optimizers.py
+++ NeuralNetworks/implementation/optimizers.py
@@ -202,3 +202,144 @@ class adagrad(Optimizer):
                 )
             print("Epoch:", current_solution)
         return current_solution
+
+
+class rmsprop(Optimizer):
+    def optimize(
+        self,
+        X,
+        y,
+        initial_solution,
+        calculate_gradient,
+        learning_rate=0.01,
+        squared_gradient_decay=0.99,
+        max_num_epoch=1000,
+        batch_size=1,
+        batch_fraction=None,
+        epsilon=1e-8,
+    ):
+        """
+        Performs RMSProp optimization.
+
+        Parameters:
+        - X: Input data.
+        - y: Target labels.
+        - initial_solution: Initial solution for optimization.
+        - calculate_gradient: Function to calculate the gradient.
+        - learning_rate: Learning rate for updating the solution (default: 0.01).
+        - squared_gradient_decay: Decay rate for the squared gradient (default: 0.99).
+        - max_num_iters: Maximum number of iterations (default: 1000).
+        - batch_size: Size of the mini batch (default: 1).
+        - batch_fraction: Fraction of the data to use in each mini batch (default: None).
+        - epsilon: Small value to avoid division by zero (default: 1e-8).
+
+        Returns:
+        - The optimized solution.
+        """
+
+        X, y = self.transfer_data_to_numpy(X, y)
+        batch_size, iterations = self.calculate_batch_size_and_iteration(
+            batch_size, batch_fraction, X
+        )
+
+        current_solution = initial_solution
+        squared_gradients = np.zeros_like(initial_solution)
+
+        for _ in range(max_num_epoch):
+            N, _ = X.shape
+            shuffled_idx = np.random.permutation(N)
+            X, y = X[shuffled_idx], y[shuffled_idx]
+            for idx in range(iterations):
+                X_selected, y_selected = (
+                    X[idx * batch_size : (idx + 1) * batch_size],
+                    y[idx * batch_size : (idx + 1) * batch_size],
+                )
+                gradient = calculate_gradient(X_selected, y_selected, current_solution)
+                squared_gradients = (
+                    squared_gradient_decay * squared_gradients
+                    + (1 - squared_gradient_decay) * gradient**2
+                )
+                current_solution = current_solution - learning_rate * gradient / (
+                    np.sqrt(squared_gradients) + epsilon
+                )
+            print("Epoch:", current_solution)
+        return current_solution
+
+
+class adam(Optimizer):
+    def optimize(
+        self,
+        X,
+        y,
+        initial_solution,
+        calculate_gradient,
+        learning_rate=0.01,
+        momentum_decay=0.9,
+        squared_gradient_decay=0.99,
+        max_num_epoch=1000,
+        batch_size=1,
+        batch_fraction=None,
+        epsilon=1e-8,
+    ):
+        """
+        Performs optimization with adam algorithm.
+
+        Parameters:
+        - X: Input data.
+        - y: Target labels.
+        - initial_solution: Initial solution for optimization.
+        - calculate_gradient: Function to calculate the gradient.
+        - learning_rate: Learning rate for updating the solution (default: 0.01).
+        - momentum_decay: Decay rate for the momentum (default: 0.9).
+        - squared_gradient_decay: Decay rate for the squared gradient (default: 0.99).
+        - max_num_iters: Maximum number of iterations (default: 1000).
+        - batch_size: Size of the mini batch (default: 1).
+        - batch_fraction: Fraction of the data to use in each mini batch (default: None).
+        - epsilon: Small value to avoid division by zero (default: 1e-8).
+
+        Returns:
+        - The optimized solution.
+        """
+
+        X, y = self.transfer_data_to_numpy(X, y)
+        batch_size, iterations = self.calculate_batch_size_and_iteration(
+            batch_size, batch_fraction, X
+        )
+
+        current_solution = initial_solution
+        momentum = np.zeros_like(initial_solution)
+        squared_gradients = np.zeros_like(initial_solution)
+        counter = 0
+
+        for _ in range(max_num_epoch):
+            N, _ = X.shape
+            shuffled_idx = np.random.permutation(N)
+            X, y = X[shuffled_idx], y[shuffled_idx]
+            for idx in range(iterations):
+                X_selected, y_selected = (
+                    X[idx * batch_size : (idx + 1) * batch_size],
+                    y[idx * batch_size : (idx + 1) * batch_size],
+                )
+                gradient = calculate_gradient(X_selected, y_selected, current_solution)
+                momentum = momentum_decay * momentum + (1 - momentum_decay) * gradient
+                squared_gradients = (
+                    squared_gradient_decay * squared_gradients
+                    + (1 - squared_gradient_decay) * gradient**2
+                )
+                counter += 1
+
+                # bias correction
+                corrected_momentum = momentum / (1 - momentum_decay**counter)
+                corrected_squared_gradients = squared_gradients / (
+                    1 - squared_gradient_decay**counter
+                )
+
+                current_solution = (
+                    current_solution
+                    - learning_rate
+                    * corrected_momentum
+                    / (np.sqrt(corrected_squared_gradients) + epsilon)
+                )
+
+            print("Epoch:", current_solution)
+        return current_solution
