c98afb781a334c12a1c6df26075ca08b65d2f036
nizwant <mateusznizwantowski@gmail.com>
2024-03-14 13:23:26 +0100

    adagrad implementation

--- NeuralNetworks/implementation/optimizers.py
+++ NeuralNetworks/implementation/optimizers.py
@@ -144,3 +144,61 @@ class mini_batch_gradient_descent_with_momentum(Optimizer):
                 current_solution = current_solution + momentum
             print("Epoch:", current_solution)
         return current_solution
+
+
+class adagrad(Optimizer):
+
+    def optimize(
+        self,
+        X,
+        y,
+        initial_solution,
+        calculate_gradient,
+        learning_rate=0.01,
+        max_num_epoch=1000,
+        batch_size=1,
+        batch_fraction=None,
+        epsilon=1e-8,
+    ):
+        """
+        Performs adagrad optimization.
+
+        Parameters:
+        - X: Input data.
+        - y: Target labels.
+        - initial_solution: Initial solution for optimization.
+        - calculate_gradient: Function to calculate the gradient.
+        - learning_rate: Learning rate for updating the solution (default: 0.01).
+        - max_num_iters: Maximum number of iterations (default: 1000).
+        - batch_size: Size of the mini batch (default: 1).
+        - batch_fraction: Fraction of the data to use in each mini batch (default: None).
+        - epsilon: Small value to avoid division by zero (default: 1e-8).
+
+        Returns:
+        - The optimized solution.
+        """
+
+        X, y = self.transfer_data_to_numpy(X, y)
+        batch_size, iterations = self.calculate_batch_size_and_iteration(
+            batch_size, batch_fraction, X
+        )
+
+        current_solution = initial_solution
+        squared_gradients = np.zeros_like(initial_solution)
+
+        for _ in range(max_num_epoch):
+            N, _ = X.shape
+            shuffled_idx = np.random.permutation(N)
+            X, y = X[shuffled_idx], y[shuffled_idx]
+            for idx in range(iterations):
+                X_selected, y_selected = (
+                    X[idx * batch_size : (idx + 1) * batch_size],
+                    y[idx * batch_size : (idx + 1) * batch_size],
+                )
+                gradient = calculate_gradient(X_selected, y_selected, current_solution)
+                squared_gradients += gradient**2
+                current_solution = current_solution - learning_rate * gradient / (
+                    np.sqrt(squared_gradients) + epsilon
+                )
+            print("Epoch:", current_solution)
+        return current_solution
