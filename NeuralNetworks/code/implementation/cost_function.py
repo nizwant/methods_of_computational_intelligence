f3aa4fce896a0e7159c0c56930b7928e7bfa6859
nizwant <mateusznizwantowski@gmail.com>
2024-03-22 14:45:25 +0100

    testing and debugging cost function

--- NeuralNetworks/code/implementation/cost_function.py
+++ NeuralNetworks/code/implementation/cost_function.py
@@ -16,16 +16,24 @@ class MeanSquaredError(CostFunction):
     def cost(self, y_hat: np.ndarray, y: np.ndarray) -> float:
         return np.mean((y - y_hat) ** 2)
 
-    def cost_derivative(self, y_hat: np.ndarray, y: np.ndarray) -> float:
-        return 2 * np.mean(y_hat - y, axis=1)
+    def cost_derivative(self, y_hat: np.ndarray, y: np.ndarray) -> np.ndarray:
+        return 2 * (y_hat - y)
 
 
 class AbsoluteError(CostFunction):
     def cost(self, y_hat: np.ndarray, y: np.ndarray) -> float:
         return np.mean(np.abs(y - y_hat))
 
-    def cost_derivative(self, y_hat: np.ndarray, y: np.ndarray) -> float:
-        return np.mean(np.sign(y_hat - y))
+    def cost_derivative(self, y_hat: np.ndarray, y: np.ndarray) -> np.ndarray:
+        return np.sign(y_hat - y)
+
+
+class CrossEntropyWithSoftmax(CostFunction):
+    def cost(self, y_hat: np.ndarray, y: np.ndarray) -> float:
+        return -np.mean(y * np.log(y_hat))
+
+    def cost_derivative(self, y_hat: np.ndarray, y: np.ndarray) -> np.ndarray:
+        return y_hat - y
 
 
 # class CrossEntropy(CostFunction):
@@ -34,11 +42,3 @@ class AbsoluteError(CostFunction):
 
 #     def cost_derivative(self, y_hat: np.ndarray, y: np.ndarray) -> np.ndarray:
 #         return (y_hat - y) / (y_hat * (1 - y_hat))
-
-
-# class CrossEntropyWithSoftmax(CostFunction):
-#     def cost(self, y_hat: np.ndarray, y: np.ndarray) -> float:
-#         return -np.mean(y * np.log(y_hat))
-
-#     def cost_derivative(self, y_hat: np.ndarray, y: np.ndarray) -> np.ndarray:
-#         return y_hat - y
